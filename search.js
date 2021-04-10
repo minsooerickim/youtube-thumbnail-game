a059a58ab1da90f54dd1d3eda74d7bf6ff32bd4b
Minsoo Kim <minsooerickim@gmail.com>
2021-04-09 22:43:06 -0700

    point system and multiple questions implemented

--- search.js
+++ search.js
@@ -1,11 +1,12 @@
 //appending thumbnail pics prints in order
+//AIzaSyBSlP4gPCojbCgP0kns_XgvDAa38-sx4vg   (futuretubies)
+//AIzaSyBYRDGmU8l00ww9MrHfT_xYg4swNBw7iNM (minsooerickim)
 function keyWordSearch() {
     gapi.client.setApiKey('AIzaSyBYRDGmU8l00ww9MrHfT_xYg4swNBw7iNM');
     gapi.client.load('youtube', 'v1', function() {
             makeRequest();
             makeRequestViews();
     });
-    // next();
 }
 
 function makeRequest() {
@@ -100,76 +101,224 @@ function parseImage(dict) {
     }
     createImage(arr, arrIds);
 }
+
+let orderedIds = [];
+let orderedViews = [];
 //inserting thumbnail
 function createImage(arr, arrIds) {
     for (var i = 0; i < arr.length; ++i) {
         img = document.createElement('img');
         img.src = arr[i];
         img.id = arrIds[i];
+        img.classList.add(i.toString());
         // img.onclick = mostViews(this.id);
         document.getElementById('thumb-container').appendChild(img);
     }
 }
 
+var firstTime = 0;
 function remove(el) {
-    var element = el;
-    element.remove();
+    if (firstTime == 0) {
+        //removing title
+        var element = document.getElementById('logo');
+        element.remove();
+
+    //replacing start to next
+        button = document.getElementById('start-img');
+        button.src = "imgs/next.png";
+    }
+    firstTime = firstTime + 1;
 }
 
+var correct = 0;
 function next() {
+    var one = document.getElementsByClassName("0")[0].id;
+    var two = document.getElementsByClassName("1")[0].id;
+    var three = document.getElementsByClassName("2")[0].id;
+
+    //ordering array as shown on page
+    if (one == bruhId[0]) {
+        orderedIds.push(bruhId[0])
+        orderedViews.push(bruhView[0]);
+    }
+    else {
+        if (one == bruhId[1]) {
+            orderedIds.push(bruhId[1])
+            orderedViews.push(bruhView[1])
+        }
+        else {
+            orderedIds.push(bruhId[2]);
+            orderedViews.push(bruhView[2]);
+        }
+    }
+
+    if (two == bruhId[1]) {
+        orderedIds.push(bruhId[1]);
+        orderedViews.push(bruhView[1]);
+    }
+    else {
+        if (two == bruhId[0]) {
+            orderedIds.push(bruhId[0]);
+            orderedViews.push(bruhView[0]);
+        }
+        else {
+            orderedIds.push(bruhId[2]);
+            orderedViews.push(bruhView[2]);
+        }
+    }
+
+    if (three == bruhId[2]) {
+        orderedIds.push(bruhId[2]);
+        orderedViews.push(bruhView[2]);
+    }
+    else {
+        if (three == bruhId[0]) {
+            orderedIds.push(bruhId[0]);
+            orderedViews.push(bruhView[0]);
+        }
+        else {
+            orderedIds.push(bruhId[1]);
+            orderedViews.push(bruhView[1]);
+        }
+    }
+
+    //get index with most views
     var winningIndex = getWinningIndex();
+
+    console.log(orderedIds);
+    console.log(orderedViews);
     console.log(winningIndex);
 
-    console.log(thumbDict);
-    console.log(bruhId);
-    console.log(bruhView);
-
-    var thumb0 = document.getElementById(bruhId[0]);
-    var thumb1 = document.getElementById(bruhId[1]);
-    var thumb2 = document.getElementById(bruhId[2]);
+    var thumb0 = document.getElementById(orderedIds[0]);
+    var thumb1 = document.getElementById(orderedIds[1]);
+    var thumb2 = document.getElementById(orderedIds[2]);
     
     thumb0.onclick = function() {
         thumb0.classList.add("blur");
         thumb1.classList.add("blur");
         thumb2.classList.add("blur");
-        console.log(bruhView[0]);
-
-
-            img = document.createElement('img');
-            img.src = "imgs/check.png";
-            img.classList.add(".correct1");
-
-            document.body.appendChild(img);
-            // document.getElementById('thumb-container').appendChild(img);
+            
         if (winningIndex == 0) {
-            //dynamically add fading filters (red, green) for wrong correct
+            img = document.createElement('span');
+            img.classList.add("correct1");
+            document.getElementById('thumb-container').appendChild(img);
+            correct = 1;
         }
-        //some function to display if correct or wrong
+        else {
+            correct = 0;
+            img = document.createElement('span');
+            img.classList.add("wrong1");
+            document.getElementById('thumb-container').appendChild(img);
+
+            if (winningIndex == 1) {
+                img = document.createElement('span');
+                img.classList.add("correct2");
+                document.getElementById('thumb-container').appendChild(img);
+            }
+            else {
+                img = document.createElement('span');
+                img.classList.add("correct3");
+                document.getElementById('thumb-container').appendChild(img);
+            }
+        }
     }
     thumb1.onclick = function() {
         thumb0.classList.add("blur");
         thumb1.classList.add("blur");
         thumb2.classList.add("blur");
-        console.log(bruhView[1]);
-        //some function to display if correct or wrong
+
+        if (winningIndex == 1) {
+            correct = 1;
+            img = document.createElement('span');
+            img.classList.add("correct2");
+            document.getElementById('thumb-container').appendChild(img);
+        }
+        else {
+            correct = 0;
+            img = document.createElement('span');
+            img.classList.add("wrong2");
+            document.getElementById('thumb-container').appendChild(img);
+
+            if (winningIndex == 0) {
+                img = document.createElement('span');
+                img.classList.add("correct1");
+                document.getElementById('thumb-container').appendChild(img);
+            }
+            else {
+                img = document.createElement('span');
+                img.classList.add("correct3");
+                document.getElementById('thumb-container').appendChild(img);                
+            }
+        }
     }
     thumb2.onclick = function() {
-
         thumb0.classList.add("blur");
         thumb1.classList.add("blur");
         thumb2.classList.add("blur");
-        console.log(bruhView[2]);
-        //some function to display if correct or wrong
-    }
-    
-}
 
+        if (winningIndex == 2) {
+            correct = 1;
+            img = document.createElement('span');
+            img.classList.add("correct3");
+            document.getElementById('thumb-container').appendChild(img);
+        }
+        else {
+            correct = 0;
+            img = document.createElement('span');
+            img.classList.add("wrong3");
+            document.getElementById('thumb-container').appendChild(img);
+
+            if (winningIndex == 0) {
+                img = document.createElement('span');
+                img.classList.add("correct1");
+                document.getElementById('thumb-container').appendChild(img);               
+            }
+            else {
+                img = document.createElement('span');
+                img.classList.add("correct2");
+                document.getElementById('thumb-container').appendChild(img);                
+            }
+        }
+    }
+}
 function getWinningIndex() {
     var highest = 0;
     for (var i = 1; i < 3; ++i) {
-        if (parseInt(bruhView[i]) > parseInt(bruhView[highest])) {
+        if (parseInt(orderedViews[i]) > parseInt(orderedViews[highest])) {
             highest = i;
         }
     }
     return highest;
+}
+
+var counter = 1;
+function removeThumbs() {
+    //clearing previous thumbs
+    document.getElementById('thumb-container').innerHTML = "";
+    bruhId = [];
+    bruhView = [];
+    thumbDict = {};
+    orderedIds = [];
+    orderedViews = [];
+
+    //restating placeholder value
+    placeHolder = getElementById('query');
+    placeHolder.placeholder = "TYPE A NEW WORD OR PHRASE HERE:)" //check if this works
+
+    //adding points
+    if (counter == 1) {
+        point = document.createElement('img');
+        point.setAttribute('id', 'pointSystem');
+        point.src = "imgs/points/" + (counter - 1).toString() + ".png";
+        document.getElementById('points').appendChild(point);
+        counter += 1;
+    }
+    if (correct == 1) {
+        point = document.getElementById("pointSystem");
+        point.src = "imgs/points/" + (counter - 1).toString() + ".png";
+
+        console.log(counter);
+        counter = counter + 1;
+        correct = 0;
+    }
 }
